fbd07a8b2cc0080aa13f0959cce3fea700841fa3
Son Tran Thanh <286.trants@gmail.com>
2024-07-04 13:40:45 +0700

    feat(docker): Setup docker starter for PHP applications

---
+++ compose/src/index.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Copyright (c) 2024 Focela Technologies. All rights reserved.
+ * Use of this source code is governed by a MIT style
+ * license that can be found in the LICENSE file.
+ */
+
+phpinfo();
